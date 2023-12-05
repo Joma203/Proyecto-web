abcff68f3f214ac2499b1d8fa8b3854f297038b7
MarcoArana <00037822@uca.edu.sv>
2023-12-04 18:08:09 -0600

    fix: image not showing

--- Client/src/components/Login/Login.jsx
+++ Client/src/components/Login/Login.jsx
@@ -44,7 +44,7 @@ const Login = () => {
             </NavLink>
             <div className="text-white font-link flex flex-col justify-center items-center gap-8 p-10">
                 <figure className="w-64 h-64 overflow-hidden rounded-full">
-                    <img src="./src/assets/logo-rojo.png" alt="logo" className="w-full h-full object-cover" />
+                    <img src="/src/assets/logo-rojo.png" alt="logo" className="w-full h-full object-cover" />
                 </figure>
                 <p className="text-3xl font-bold">Inicio de sesión</p>
 
--- Client/src/components/MainPage/MainPage.jsx
+++ Client/src/components/MainPage/MainPage.jsx
@@ -5,7 +5,7 @@ const MainPage = () => {
         <section>
             <header className="bg-main-2 w-full flex flex-row justify-around">
                 <figure className="w-60 h-40 overflow-hidden">
-                    <img src="./src/assets/logo-rojo.png" alt="Logo" className="w-full h-full object-cover" />
+                    <img src="/src/assets/logo-rojo.png" alt="Logo" className="w-full h-full object-cover" />
                 </figure>
 
                 <div className="flex flex-row gap-8 items-center">
@@ -22,7 +22,7 @@ const MainPage = () => {
 
                 <div className="grid grid-cols-2 px-20 py-8 justify-items-center items-center">
                     <figure className="w-80 h-60 overflow-hidden">
-                        <img src="./src/assets/logo-morado.png" alt="Math image" className="w-full h-full object-cover" />
+                        <img src="/src/assets/logo-morado.png" alt="Math image" className="w-full h-full object-cover" />
                     </figure>
                     <p className="text-white font-link text-center">
                         FUNUM es una aplicación web dedicada a la enseñanza del mundo de las matemáticas. La web ha sido desarrollada por alumnos de segundo año de la carrera Ingeniería Informática en la Universidad Centroamericana José Simeón Cañas (UCA).
@@ -34,7 +34,7 @@ const MainPage = () => {
                         La página consta de varias lecciones de la materia de matemáticas, junto a estas, se presenta un examen con el cual el estudiante podrá poner a prueba su conocimiento de acuerdo a la laección que acaba de leer. Para incentivar al usuario a mantenerse activo en la página, se ha diseñado un sistema de puntos, por el cual los usuarios pueden competir en un ranking para ver quien tiene más puntos, además de tener una tienda de avatares donde se pueden canjear estos puntos.
                     </p>
                     <figure className="w-80 h-60 overflow-hidden">
-                        <img src="./src/assets/math2.jpg" alt="Math image" className="w-full h-full object-cover" />
+                        <img src="/src/assets/math2.jpg" alt="Math image" className="w-full h-full object-cover" />
                     </figure>
                 </div>
 
